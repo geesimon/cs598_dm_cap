52c842e77b7ee8ae51c03bae97ed21fbbe505888
geesimon <geesimon@hotmail.com>
2019-07-03 18:30:59 +0800

    Complete sentence breaking and sentiment analysis

--- task4/test.py
+++ task4/test.py
@@ -1,41 +1,49 @@
 import pickle
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from tqdm import tqdm
 
-def splitParagraphIntoSentences(paragraph):
-    import re
-    sentenceEnders = re.compile(r"""
-        # Split sentences on whitespace between them.
-        (?:               # Group for two positive lookbehinds.
-          (?<=[.!?])      # Either an end of sentence punct,
-        | (?<=[.!?]['"])  # or end of sentence punct and quote.
-        )                 # End group of two positive lookbehinds.
-        (?<!  Mr\.   )    # Don't end sentence on "Mr."
-        (?<!  Mrs\.  )    # Don't end sentence on "Mrs."
-        (?<!  Jr\.   )    # Don't end sentence on "Jr."
-        (?<!  Dr\.   )    # Don't end sentence on "Dr."
-        (?<!  Prof\. )    # Don't end sentence on "Prof."
-        (?<!  Sr\.   )    # Don't end sentence on "Sr."
-        \s+               # Split on whitespace between sentences.
-        """, 
-        re.IGNORECASE | re.VERBOSE)
-    sentenceList = sentenceEnders.split(paragraph)
-    return sentenceList
+MAX_DISH_COUNT = 100
 
 with open('./categories/Chinese.pkl', 'rb') as f:
-    cuisine_reviews = pickle.load(f)
-
+    cuisine_reviews = pickle.load(f)    
 with open('ChineseDishes.txt', 'r') as f:
-    dishes = {line.replace("\n", ""):{} for line in f.readlines()[0:100]}
+    dishes = {line.replace("\n", "").lower() : {} for line in f.readlines()[0:MAX_DISH_COUNT]}
 
-for _dish in dishes:
+for _review in tqdm(cuisine_reviews):
+    _review['sentences'] = nltk.sent_tokenize(_review['text'])
+
+
+sia = SentimentIntensityAnalyzer()
+
+for _dish in tqdm(dishes):
+    dishes[_dish] = {'rest2review':{}, 'review2polarity':{}, 'rest2polarity':{}}
     _dish2rest = set()
-    _total_count = 0
+    _review_count = 0
+    
     for _review in cuisine_reviews:
-        _count = _review['text'].count(_dish)
-        if _count > 0:
-            _total_count += _count
-            if _review['business_id'] not in _dish2rest:
-                _dish2rest.add(_review['business_id'])
+        _review_id = _review['review_id']
+        _rest_id = _review['business_id']
+        _review_polarity = 0
+        _review_sentence_count = 0
+        for _sentence in _review['sentences']:
+            _count = _sentence.count(_dish)
+            if _count > 0:
+                _review_sentence_count += 1
+                _ss = sia.polarity_scores(_sentence)
+                _review_polarity += _ss['compound']
+        
+        if _review_sentence_count > 0:
+            _review_polarity = _review_polarity / _review_sentence_count
+            _review_count += 1
+            dishes[_dish]['review2polarity'][_review_id] = _review_polarity
+            if _rest_id in dishes[_dish]['rest2review']:
+                dishes[_dish]['rest2review'][_rest_id].add(_review_id)
+                dishes[_dish]['rest2polarity'][_rest_id] += _review_polarity
+            else:
+                dishes[_dish]['rest2review'][_rest_id] = set([_review_id])
+                dishes[_dish]['rest2polarity'][_rest_id] = _review_polarity
     
-    
-    print("%s:%d:%d"%(_dish, _total_count, len(_dish2rest)))
+            
+    for _id in dishes[_dish]['rest2polarity']:
+        dishes[_dish]['rest2polarity'][_id] /= len(dishes[_dish]['rest2review'][_id])
